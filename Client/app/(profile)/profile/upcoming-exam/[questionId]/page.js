025593a2d18f08b8e03b5146650ba3f28c053478
ABDUR-RAHIM1 <abdurrahim88557@gmail.com>
2025-01-05 00:10:01 +0600

    future Q hide

--- Client/app/(profile)/profile/upcoming-exam/[questionId]/page.js
+++ Client/app/(profile)/profile/upcoming-exam/[questionId]/page.js
@@ -169,107 +169,121 @@ export default function ExamPage({ params }) {
     const seconds = timeLeft % 60;
 
     return (
-        <div className="min-h-screen flex items-center justify-center relative">
-            {/* Exam Timer */}
-            <div className="w-auto py-5 md:py-10 px-2 md:px-5 bg-gray-900 bg-opacity-95 rounded-md fixed top-[50%] right-0">
-                <h3 className="text-gray-300 flex items-center gap-2">
-                    <span>{selectQuesCount}</span>
-                    <strong className="text-blue-500">/</strong>
-                    {totalQuesCount}
-                </h3>
-                <h4 className="text-xl text-gray-300 mt-3">
-                    {minutes}:{seconds < 10 ? `0${seconds}` : seconds} <span className="mx-1">M</span>
-                </h4>
-            </div>
+        <div>
+            < div className="min-h-screen flex items-center justify-center relative">
+                {/* Exam Timer */}
+                {
+                    dateStatus === "future" || timeStatus === "future" ? null :
+                        < div className="w-auto py-5 md:py-10 px-2 md:px-5 bg-gray-900 bg-opacity-95 rounded-md fixed top-[50%] right-0">
+                            <h3 className="text-gray-300 flex items-center gap-2">
+                                <span>{selectQuesCount}</span>
+                                <strong className="text-blue-500">/</strong>
+                                {totalQuesCount}
+                            </h3>
+                            <h4 className="text-xl text-gray-300 mt-3">
+                                {minutes}:{seconds < 10 ? `0${seconds}` : seconds} <span className="mx-1">M</span>
+                            </h4>
+                        </div>
+                }
 
-            <div className="bg-white shadow-lg rounded-lg max-w-4xl w-full">
-                <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">Question Paper</h1>
+                <div className="bg-white shadow-lg rounded-lg max-w-4xl w-full">
+                    <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">Question Paper</h1>
 
-                <div className=" my-3 text-center">
+                    <div className=" my-3 text-center">
 
-                    {
-                        // Check for date and time conditions and display appropriate message
-                        dateStatus === "past" ?
-                            <p className=" text-red-500 fadeIn animate-delay-1s">পরিক্ষার নির্ধারিত তারিখ শেষ হয়েছে!</p> :
-                            dateStatus === "future" ?
-                                <p className=" text-yellow-600 fadeIn animate-delay-1s">পরিক্ষার তারিখ এখনো আসেনি!</p> :
-                                timeStatus === "past" ?
-                                    <p className=" text-red-500 fadeIn animate-delay-1s">পরিক্ষার সময় শেষ হয়ে গেছে!</p> :
-                                    timeStatus === "future" ?
-                                        <p className=" text-yellow-500 fadeIn animate-delay-1s">পরিক্ষার সময় এখনো আসেনি!</p> :
-                                        <p className=" text-green-800 fadeIn animate-delay-1s">আপনি এখন পরীক্ষায় অংশগ্রহণ করতে পারবেন।</p>
-                    }
+                        {
+                            // Check for date and time conditions and display appropriate message
+                            dateStatus === "past" ?
+                                <p className=" text-red-500 fadeIn animate-delay-1s">পরিক্ষার নির্ধারিত তারিখ শেষ হয়েছে!</p> :
+                                dateStatus === "future" ?
+                                    <p className=" text-yellow-600 fadeIn animate-delay-1s">পরিক্ষার তারিখ এখনো আসেনি!</p> :
+                                    timeStatus === "past" ?
+                                        <p className=" text-red-500 fadeIn animate-delay-1s">পরিক্ষার সময় শেষ হয়ে গেছে!</p> :
+                                        timeStatus === "future" ?
+                                            <p className=" text-yellow-500 fadeIn animate-delay-1s">পরিক্ষার সময় এখনো আসেনি!</p> :
+                                            <p className=" text-green-800 fadeIn animate-delay-1s">আপনি এখন পরীক্ষায় অংশগ্রহণ করতে পারবেন।</p>
+                        }
 
-                </div>
+                    </div>
 
-                <div className="border-b-2 pb-4 mb-8 text-gray-700">
-                    <table className="table-auto w-full text-left border-collapse border border-gray-300 rounded-md">
-                        <tbody>
-                            <tr className="border-b border-gray-200">
-                                <td className="font-semibold py-2 px-4 border-r border-gray-200">Category:</td>
-                                <td className="py-2 px-4">{formData.questionCategory}</td>
-                            </tr>
-                            <tr className="border-b border-gray-200">
-                                <td className="font-semibold py-2 px-4 border-r border-gray-200">Course:</td>
-                                <td className="py-2 px-4">{formData.questionTitle}</td>
-                            </tr>
-                            <tr className="border-b border-gray-200">
-                                <td className="font-semibold py-2 px-4 border-r border-gray-200">Exam Date:</td>
-                                <td className="py-2 px-4">{new Date(formData.examDate).toLocaleDateString("en-US")}</td>
-                            </tr>
-                            <tr className="border-b border-gray-200">
-                                <td className="font-semibold py-2 px-4 border-r border-gray-200">Exam Time:</td>
-                                <td className="py-2 px-4">{formData.examTime}</td>
-                            </tr>
-                            <tr>
-                                <td className="font-semibold py-2 px-4 border-r border-gray-200">Duration:</td>
-                                <td className="py-2 px-4">{formData.examDuration} minutes</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                    <div className="border-b-2 pb-4 mb-8 text-gray-700">
+                        <table className="table-auto w-full text-left border-collapse border border-gray-300 rounded-md">
+                            <tbody>
+                                <tr className="border-b border-gray-200">
+                                    <td className="font-semibold py-2 px-4 border-r border-gray-200">Category:</td>
+                                    <td className="py-2 px-4">{formData.questionCategory}</td>
+                                </tr>
+                                <tr className="border-b border-gray-200">
+                                    <td className="font-semibold py-2 px-4 border-r border-gray-200">Course:</td>
+                                    <td className="py-2 px-4">{formData.questionTitle}</td>
+                                </tr>
+                                <tr className="border-b border-gray-200">
+                                    <td className="font-semibold py-2 px-4 border-r border-gray-200">Exam Date:</td>
+                                    <td className="py-2 px-4">{new Date(formData.examDate).toLocaleDateString("en-US")}</td>
+                                </tr>
+                                <tr className="border-b border-gray-200">
+                                    <td className="font-semibold py-2 px-4 border-r border-gray-200">Exam Time:</td>
+                                    <td className="py-2 px-4">{formData.examTime}</td>
+                                </tr>
+                                <tr>
+                                    <td className="font-semibold py-2 px-4 border-r border-gray-200">Duration:</td>
+                                    <td className="py-2 px-4">{formData.examDuration} minutes</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
-                <div>
-                    {formData !== null &&
-                        formData.questions.map((item, index) => (
-                            <div key={index} className="my-6 p-5 bg-gray-50 border-l-4 border-blue-500 rounded-md shadow-sm">
-                                <h2 className="text-xl font-semibold text-gray-800 mb-4">
-                                    <span className="text-blue-500">{index + 1}.</span> {item.questionText}
-                                </h2>
-                                <div className="space-y-2">
-                                    {item.options.map((o, idx) => (
-                                        <div key={idx} className="flex items-center">
-                                            <input
-                                                type="radio"
-                                                name={item.questionId}
-                                                value={o}
-                                                id={`${item.questionId}-${idx}`}
-                                                className="w-5 h-5 text-blue-600 focus:ring-blue-500 border-gray-300"
-                                                onChange={(e) => handleChange(idx, item.questionId)}
-                                            />
-                                            <label htmlFor={`${item.questionId}-${idx}`} className="ml-3 text-gray-700">
-                                                {o}
-                                            </label>
-                                        </div>
-                                    ))}
+                    <div>
+                        {
+
+                            dateStatus === "future" || timeStatus === "future" ?
+                                <div className=" w-full min-h-[50vh] flex items-center justify-center">
+                                    <h2 className=" text-red-500">প্রশ্ন নির্ধারিত সময়ে চলে আসবে</h2>
                                 </div>
-                            </div>
-                        ))}
+                                :
+
+                                formData !== null &&
+                                formData.questions.map((item, index) => (
+                                    <div key={index} className="my-6 p-5 bg-gray-50 border-l-4 border-blue-500 rounded-md shadow-sm">
+                                        <h2 className="text-xl font-semibold text-gray-800 mb-4">
+                                            <span className="text-blue-500">{index + 1}.</span> {item.questionText}
+                                        </h2>
+                                        <div className="space-y-2">
+                                            {item.options.map((o, idx) => (
+                                                <div key={idx} className="flex items-center">
+                                                    <input
+                                                        type="radio"
+                                                        name={item.questionId}
+                                                        value={o}
+                                                        id={`${item.questionId}-${idx}`}
+                                                        className="w-5 h-5 text-blue-600 focus:ring-blue-500 border-gray-300"
+                                                        onChange={(e) => handleChange(idx, item.questionId)}
+                                                    />
+                                                    <label htmlFor={`${item.questionId}-${idx}`} className="ml-3 text-gray-700">
+                                                        {o}
+                                                    </label>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                ))}
+                    </div>
+
+                    <button
+                        disabled={dateStatus === "future" || timeStatus === "future"}
+                        onClick={handleQuestionSubmit}
+                        className={`mt-6 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semibold py-3 px-6 rounded-lg shadow-md transform transition duration-300 w-full ${dateStatus === "future" || timeStatus === "future" ? 'cursor-not-allowed opacity-50' : 'hover:shadow-lg hover:scale-105'}`}
+                    >
+                        {loading
+                            ? "Loading . . . "
+                            : (dateStatus === "future" || timeStatus === "future")
+                                ? "অপেক্ষা করুন"
+                                : "সাবমিট করুন"}
+                    </button>
+
                 </div>
-
-                <button
-                    disabled={dateStatus === "future" || timeStatus === "future"}
-                    onClick={handleQuestionSubmit}
-                    className={`mt-6 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semibold py-3 px-6 rounded-lg shadow-md transform transition duration-300 w-full ${dateStatus === "future" || timeStatus === "future" ? 'cursor-not-allowed opacity-50' : 'hover:shadow-lg hover:scale-105'}`}
-                >
-                    {loading
-                        ? "Loading . . . "
-                        : (dateStatus === "future" || timeStatus === "future")
-                            ? "অপেক্ষা করুন"
-                            : "সাবমিট করুন"}
-                </button>
-
             </div>
-        </div>
+
+        </div >
     );
 }
